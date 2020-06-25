b631cae1ea63db59dede51a3fbc5707afe0c3e08
Kanav Khanna <40358658+kanav-khanna@users.noreply.github.com>
2020-06-25 20:45:27 +0530

    created homepage components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
-
+//just a test
 import HomePage from './pages/homepage/homepage.component';
 
 const HatsPage = () => (
