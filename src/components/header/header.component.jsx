ca70de05a0f59577ee43bf92eeb55071ebd8f6b6
Kanav Khanna <40358658+kanav-khanna@users.noreply.github.com>
2020-07-02 12:26:28 +0530

    adding sign in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 import './header.styles.scss';
-import {Link} from 'react-router-dom';      
-import {ReactComponent as Logo} from '../../assets/crown.svg';
 
-const Header =() => (
-    <div className ='header'>
-
-        <Link className='logo-container' to="/">
-            <Logo className = 'logo'></Logo>
-        </Link>
-            <div>
-                <Link className ='option' to='/shop'>
-                    SHOP
-                </Link>
-                <Link className ='option' to='/shop'>
-                    CONTACT
-                </Link>
-                 
-            </div>
+const Header = () => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
     </div>
-
-)
+  </div>
+);
 
 export default Header;
